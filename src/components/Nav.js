c348824ae804cd4d99642a480e0eff5eb4e151af
ambar-ml <68280445+ambar-ml@users.noreply.github.com>
2021-07-05 17:19:53 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -93,7 +93,7 @@ function Nav() {
 
         <div >
 
-            <nav className="navbar navbar-expand navbar-dark bg-dark navbar-default navbar-static-top">
+            <nav className="navbar navbar-expand navbar-dark bg-dark navbar-default navbar-fixed">
                 <div className="container-fluid">
                     
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
